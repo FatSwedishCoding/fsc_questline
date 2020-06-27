8a4cb2d01614eb96ec47de7b594806c662c2d019
Fat Swedish Coding <marcusf1993@hotmail.com>
2020-06-28 01:30:29 +0200

    Add files via upload

---
+++ questline.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 28 jun 2020 kl 01:29
+-- Serverversion: 10.4.6-MariaDB
+-- PHP-version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `coloursnetdev`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `questline`
+--
+
+CREATE TABLE `questline` (
+  `identifier` varchar(255) NOT NULL,
+  `timeleft` int(11) NOT NULL,
+  `time` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `questline`
+--
+ALTER TABLE `questline`
+  ADD PRIMARY KEY (`identifier`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
